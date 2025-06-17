2dc71d3555c4531a63e6ae6c6c122c177461d232
alhareth alhareth <alharethalalem@gmail.com>
2025-06-17 12:02:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/theses/ThesesClientPage.tsx
+++ src/app/theses/ThesesClientPage.tsx
@@ -14,6 +14,7 @@ import { searchTheses as apiSearchTheses, archiveThesis as apiArchiveThesis } fr
 import { useToast } from '@/hooks/use-toast';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Skeleton } from '@/components/ui/skeleton';
+import { Card } from '@/components/ui/card';
 
 
 interface ThesesClientPageProps {
